e3f8df747fd7e814652361f03810e14e5022d6a1
Alex Zauzolkov <alex.zauzolkov@yandex.ru>
2021-11-16 14:06:19 +1000

    Remove obsolete code

--- HighROV.cpp
+++ HighROV.cpp
@@ -11,14 +11,14 @@
 #include "USB/USBAPI.h"
 #include "AnalogSensors.h"
 
-// IMUSensor imu;
 rov::RovControl control;
 rov::RovTelimetry telimetry;
 
 void HighROV::init() {
-    SerialUSB.println("HighROV init! (SerialUSB)");
+    SerialUSB.println("HighROV init!");
 
     pinMode(LED_BUILTIN, OUTPUT);
+    analogWrite(LED_BUILTIN, 100);
 
     PWMController::init();
     Networking::init();
@@ -29,7 +29,7 @@ void HighROV::init() {
     IMUSensor::init();
     AnalogSensors::init();
 
-    // delay(5000);
+    delay(3000);
 }
 
 void debug(rov::RovControl &ctrl) {
@@ -48,87 +48,26 @@ void debug(rov::RovControl &ctrl) {
     PWMController::set_servo_power(servos::pwm_a3, ctrl.thrusterPower[9]);
 }
 
-void test_peripherals() {
-    static uint64_t counter = 0;
-    counter++;
-
-    int p = (counter % 200) - 100;
-
-    PWMController::set_thruster(config::thrusters::horizontal_front_left, p);
-    PWMController::set_thruster(config::thrusters::horizontal_front_right, p);
-    PWMController::set_thruster(config::thrusters::horizontal_back_left, p);
-    PWMController::set_thruster(config::thrusters::horizontal_back_right, p);
-    PWMController::set_thruster(config::thrusters::vertical_front, p);
-    PWMController::set_thruster(config::thrusters::vertical_back, p);
-    PWMController::set_thruster(config::thrusters::custom_0, p);
-    PWMController::set_thruster(config::thrusters::custom_1, p);
-
-    int angle = (counter % 200) > 100 ? 2 : -2;
-
-    PWMController::set_servo_angle(config::servos::front, angle);
-    PWMController::set_servo_angle(config::servos::back, angle);
-    PWMController::set_servo_angle(config::servos::pwm_a2, angle);
-    PWMController::set_servo_angle(config::servos::pwm_a3, angle);
-
-    int manip_p = p / 5 >  10 ?  1 :
-                  p / 5 < -10 ? -1 : 0;
-
-    Manipulator::set_power(manip_p, manip_p);
-}
-
 void HighROV::run() {
-
-    /* TODO: A2, A3 - thrusters */
-
-
-    // test_peripherals();
-    // imu.update();
-
-    // SerialUSB.print("Yaw: ");
-    // SerialUSB.print(IMUSensor::inst().getYaw());
-    // SerialUSB.print("\tPitch: ");
-    // SerialUSB.print(IMUSensor::inst().getRoll());
-    // SerialUSB.print("\tRoll: ");
-    // SerialUSB.print(IMUSensor::inst().getPitch());
-
-    // // digitalWrite(LED_BUILTIN, HIGH);
-    // // delay(5);
-    // // digitalWrite(LED_BUILTIN, LOW);
-    // // delay(5);
-
-    // SerialUSB.print("\tDepth: ");
-    // SerialUSB.println(DepthSensor::get_depth());
-
-
-
-    // SerialUSB.println(p);
-
-    // WiFiUpdater::check_updates();
-
-    // digitalWrite(LED_BUILTIN, millis() % 100 < 50);
+    AnalogSensors::update();
 
     telimetry.yaw = IMUSensor::getYaw();
     telimetry.roll = IMUSensor::getRoll();
     telimetry.pitch = IMUSensor::getPitch();
     telimetry.depth = DepthSensor::getDepth();
-    // TODO: telimetry.temperature = DepthSensor::getTemp();
+    telimetry.temperature = DepthSensor::getTemp();
+    telimetry.ammeter = AnalogSensors::getAmperage();
     telimetry.voltmeter = AnalogSensors::getVoltage();
     telimetry.cameraIndex = RotaryCameras::get_cam_index();
 
     Networking::read_write_udp(telimetry, control);
     if (!control.debugFlag) {
         Thrusters::update_thrusters(control, telimetry);
-        RotaryCameras::set_angle(config::servos::front, constrain(control.cameraRotation[0], -1, 1));
-        RotaryCameras::set_angle(config::servos::back, constrain(control.cameraRotation[1], -1, 1));
+        RotaryCameras::set_angle(config::servos::front, constrain(control.cameraRotation[0], -1, 1) * 3.0);
+        RotaryCameras::set_angle(config::servos::back,  constrain(control.cameraRotation[1], -1, 1) * 3.0);
         RotaryCameras::select_cam(control.cameraIndex == 1 ? true : false);
         Manipulator::set_power(control.manipulatorRotation, control.manipulatorOpenClose);
-        // SerialUSB.print(control.manipulatorOpenClose); SerialUSB.print("\t");\
-        // SerialUSB.print(control.regulators); SerialUSB.print("\t");
-        // SerialUSB.print(control.desiredDepth); SerialUSB.print("\t");
-        // SerialUSB.print(control.cameraIndex); SerialUSB.print("\t");
-        // SerialUSB.println(" ");
     } else {
-        // SerialUSB.println("debugging!");
         debug(control);
     }
 
@@ -139,10 +78,5 @@ void HighROV::run() {
     }
 
     delay(20);
-
-    // imu.processImu();
-    // telimetry.yaw = imu.get_yaw();
-    // telimetry.roll = imu.get_roll();
-    // telimetry.pitch = imu.get_pitch();
 }
 
